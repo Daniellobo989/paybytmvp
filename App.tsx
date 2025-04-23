c7ddeb5df47ca3a99e24a2d1b3c335f03307c037
Daniellobo989 <cardosolobo@protonmail.com>
2025-04-23 20:47:42 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -40,3 +40,11 @@ function App() {
 }
 
 export default App;
+<a href="autenticacao.html" class="inline-block px-6 py-3 rounded-lg bg-[#002D72] hover:bg-[#001d4c] text-white font-bold shadow-md transition-all duration-300 hover:shadow-lg">
+  <div class="flex items-center">
+    <span class="mr-2">Sistema de Autenticação Anônima</span>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+    </svg>
+  </div>
+</a>
