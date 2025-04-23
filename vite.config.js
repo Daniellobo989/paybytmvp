ef4e7b6e75d1e8bf8c5535eca078e6b00c869ae9
Daniellobo989 <cardosolobo@protonmail.com>
2025-04-23 14:14:43 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() ],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+  },
   server: {
     port: 3000,
     host: '0.0.0.0',
@@ -15,4 +22,4 @@ export default defineConfig({
       }
     }
   }
-});
+}) ;
